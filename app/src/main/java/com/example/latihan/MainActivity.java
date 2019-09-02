9980572e28d8a6ed99d28ecf425dde3eeee2612c
Stephanie Nadia <stephanie.nadia@ti.ukdw.ac.id>
2019-09-02 19:33:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.latihan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//    txt.setOnClickListener(new View.OnClickListener(){
+//       @Override
+//       public void onClick(View v){
+//
+//        }
+//    });
+
+//    txt.setOnClickListener (new View.OnClickListener(){
+//        @Override
+//                public void onClick(View v){
+//            Intent i = new Intent (MainActivity.this, Activity_Kedua.class);
+//            startActivity(i);
+//        }
+//    });
+}
---
+++ app/src/main/java/com/example/latihan/Main_Activity_Utama.java
@@ -0,0 +1,34 @@
+package com.example.latihan;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Main_Activity_Utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//    txt.setOnClickListener(new View.OnClickListener(){
+//       @Override
+//       public void onClick(View v){
+//
+//        }
+//    });
+
+//    txt.setOnClickListener (new View.OnClickListener(){
+//        @Override
+//                public void onClick(View v){
+//            Intent i = new Intent (MainActivity.this, Activity_Kedua.class);
+//            startActivity(i);
+//        }
+//    });
+}
