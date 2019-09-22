53cc0bb02a06f019fe37d9fcc539ae0b78bdc75e
Stephanie Nadia <stephanie.nadia@ti.ukdw.ac.id>
2019-09-22 14:50:34 +0700

    Notifikasi 22-9-19

--- app/src/main/java/com/example/latihan/MainActivity.java
+++ app/src/main/java/com/example/latihan/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.latihan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +21,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Button btn = (Button)findViewById(R.id.btnSubmit);
+    private boolean isReciverReigtered = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +46,10 @@ public class MainActivity extends AppCompatActivity {
         // onClick of button perform this simplest code.
         if (email.matches(emailPattern))
         {
-            Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
             Intent i = new Intent(MainActivity.this, home_viewpager.class);
+            Bundle b = new Bundle();
+            b.putString("email", email);
+            i.putExtras(b);
             startActivity(i);
         }
         else
@@ -45,6 +58,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(!isNetworkAvailable(context)){
+                Toast.makeText(context, "ON",Toast.LENGTH_SHORT).show();
+                Notification(context, "Wifi Connection On");
+            }
+            else if(isNetworkAvailable(context)){
+                Toast.makeText(context, "OFF",Toast.LENGTH_SHORT).show();
+                Notification(context, "Wifi Connection OFF");
+            }
+        }
+        public void Notification(Context context, String msg){
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher_simbol)
+                    .setTicker(msg)
+                    .setContentTitle("Latihan notification wifi")
+                    .setContentText(msg)
+                    .setAutoCancel(true);
+            NotificationManager notificationmanager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationmanager.notify(0, builder.build());
+        }
+
+        private boolean isNetworkAvailable(Context context){
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null;
+        }
+    };
+    protected void onResume(){
+        super.onResume();
+        if(!isReciverReigtered){
+            isReciverReigtered = true;
+            registerReceiver(receiver, new IntentFilter("android.net.wifi.STATE_CHANGE"));
+        }
+    }
+    protected void onPause(){
+        super.onPause();
+        if(isReciverReigtered){
+
+        }
+    }
+
 //    txt.setOnClickListener(new View.OnClickListener(){
 //       @Override
 //       public void onClick(View v){
--- app/src/main/java/com/example/latihan/home_viewpager.java
+++ app/src/main/java/com/example/latihan/home_viewpager.java
@@ -1,16 +1,22 @@
 package com.example.latihan;
 
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.core.app.NotificationCompat;
 import androidx.viewpager.widget.ViewPager;
 
 
 public class home_viewpager extends AppCompatActivity {
-
+    private boolean isReciverReigtered = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,53 @@ public class home_viewpager extends AppCompatActivity {
         vpgr.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
         TabLayout tl = findViewById(R.id.tab_layout);
         tl.setupWithViewPager(vpgr);
+        Intent intent = getIntent();
+        Bundle bundle = getIntent().getExtras();
+        String strEmail = bundle.getString("email","");
+        Toast.makeText(getApplicationContext(), "Selamat datang "+strEmail,Toast.LENGTH_SHORT
+        ).show();
+    }
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(!isNetworkAvailable(context)){
+                //Toast.makeText(context, "ON",Toast.LENGTH_SHORT).show();
+                Notification(context, "Wifi Connection On");
+            }
+            else if(isNetworkAvailable(context)){
+                //Toast.makeText(context, "OFF",Toast.LENGTH_SHORT).show();
+                Notification(context, "Wifi Connection OFF");
+            }
+        }
+        public void Notification(Context context, String msg){
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setTicker(msg)
+                    .setContentTitle("Latihan notification wifi")
+                    .setContentText(msg)
+                    .setAutoCancel(true);
+            NotificationManager notificationmanager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationmanager.notify(0, builder.build());
+        }
+
+        private boolean isNetworkAvailable(Context context){
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null;
+        }
+    };
+    protected void onResume(){
+        super.onResume();
+        if(!isReciverReigtered){
+            isReciverReigtered = true;
+            registerReceiver(receiver, new IntentFilter("android.net.wifi.STATE_CHANGE"));
+        }
+    }
+    protected void onPause(){
+        super.onPause();
+        if(isReciverReigtered){
+
+        }
     }
 }
 
